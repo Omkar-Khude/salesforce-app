564c3eaaf2b277669b45dceee588e22bf7d68d8e
Omkar-Khude <126755099+Omkar-Khude@users.noreply.github.com>
2025-04-02 09:26:05 +0530

    latest

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Dashboard from './components/Dashboard';
+// import LoggedIn from './components/LoggedIn';
+// import MetadataDisplay from './components/MetadataDisplay';
+
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Dashboard />} />
+//         <Route path="/logged-in" element={<LoggedIn />} />
+//         <Route path="/metadata-display" element={<MetadataDisplay />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
+
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import LoggedIn from './components/LoggedIn';
 import MetadataDisplay from './components/MetadataDisplay';
 
-
 const App = () => {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/logged-in" element={<LoggedIn />} />
         <Route path="/metadata-display" element={<MetadataDisplay />} />
+        {/* Add a catch-all route for better 404 handling */}
+        <Route path="*" element={<Dashboard />} />
       </Routes>
     </Router>
   );
