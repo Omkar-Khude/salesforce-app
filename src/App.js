15140c76d04e5af0208553f92aa7ef661206819e
Omkar-Khude <126755099+Omkar-Khude@users.noreply.github.com>
2025-04-02 09:43:53 +0530

    latest

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Dashboard from './components/Dashboard';
-// import LoggedIn from './components/LoggedIn';
-// import MetadataDisplay from './components/MetadataDisplay';
-
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Dashboard />} />
-//         <Route path="/logged-in" element={<LoggedIn />} />
-//         <Route path="/metadata-display" element={<MetadataDisplay />} />
-//       </Routes>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom'; // Changed to HashRouter
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import LoggedIn from './components/LoggedIn';
 import MetadataDisplay from './components/MetadataDisplay';
+
 
 const App = () => {
   return (
